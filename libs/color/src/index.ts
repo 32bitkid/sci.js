1bf3d2be1990e9e0b7d73bcc57f151f6950bbfba
J Holmes <32bitkid@gmail.com>
2024-03-13 11:55:44 -0600

    remove static BW palette in favor of toGrayscale transformer

--- libs/color/src/index.ts
+++ libs/color/src/index.ts
@@ -1,6 +1,5 @@
 export { RAW_CGA } from './cga-palette';
 export { TRUE_CGA } from './true-cga-palette';
-export { BW_PALETTE } from './bw-palette';
 export { DGA_PALETTE } from './dga-palette';
 
 export { redMeanDiff } from './red-mean-diff';
@@ -15,3 +14,4 @@ export type { DitherPair } from './dither-pair';
 export type { DitherTransform } from './dither-transform';
 export * as BuiltinDitherPairs from './predefined-dither-pairs';
 export { IBM5153Dimmer } from './IBM-5153-dimmer';
+export { toGrayscale } from './to-grayscale';
---
+++ libs/color/src/to-grayscale.ts
@@ -0,0 +1,14 @@
+export const toGrayscale = (palette: Uint32Array): Uint32Array => {
+  const grayscale = new Uint32Array(16);
+  for (let i = 0; i < 16; i++) {
+    const entry = palette[i];
+    const R = (entry >>> 0) & 0xff;
+    const G = (entry >>> 8) & 0xff;
+    const B = (entry >>> 16) & 0xff;
+    const a = (entry >>> 24) & 0xff;
+    // Fast luminance approximation
+    const l = (R + R + R + B + G + G + G + G) >>> 3;
+    grayscale[i] = (a << 24) | (l << 16) | (l << 8) | l;
+  }
+  return grayscale;
+};
