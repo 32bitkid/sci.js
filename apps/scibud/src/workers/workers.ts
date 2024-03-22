43c703e0ce11310ed3cfb98459facde36bd153c0
J Holmes <32bitkid@gmail.com>
2024-03-21 20:46:09 -0600

    Chore: adding prebuild step to run tests

--- apps/scibud/src/workers/workers.ts
+++ apps/scibud/src/workers/workers.ts
@@ -5,7 +5,3 @@ import { renderPicWorker } from './render-pic-worker';
 worker({
   renderPic: renderPicWorker,
 });
-
-/*
-ffmpeg -r 60 -f image2 -s 1920x1080 -i ./out/test.%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p ./out/test.mp4
- */
