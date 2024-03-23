b19aa98563bc801588c7d2e30e82e46ffff5ad86
J Holmes <32bitkid@gmail.com>
2024-03-22 18:34:53 -0600

    Chore: adding tests for dither and palette filters
    - fixing bug

---
+++ libs/image/src/dither-filter.spec.ts
@@ -0,0 +1,25 @@
+import { generateSciDitherPairs } from '@4bitlabs/color';
+import { createDitherFilter } from './dither-filter';
+import { createIndexedPixelData } from './indexed-pixel-data';
+
+describe('dither-filter', () => {
+  it('should dither', () => {
+    const img = createIndexedPixelData(2, 2);
+    img.pixels[0] = 0x01;
+    img.pixels[1] = 0x01;
+    img.pixels[2] = 0x01;
+    img.pixels[3] = 0x01;
+
+    const filter = createDitherFilter(
+      generateSciDitherPairs(Uint32Array.of(0xff_00_00_00, 0xff_ff_00_00)),
+    );
+    const result = filter(img);
+    expect(result.data).toEqual(
+      // prettier-ignore
+      Uint8ClampedArray.of(
+        0, 0, 0xff, 0xff, 0, 0,    0, 0xff,
+        0, 0,    0, 0xff, 0, 0, 0xff, 0xff,
+      ),
+    );
+  });
+});
