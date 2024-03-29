5e1c7f39c06d1ff250fde377e1cd82deabbb51a5
J Holmes <32bitkid@gmail.com>
2024-03-28 23:15:59 -0600

    Fixed: better error message when compression method is not supported.

--- libs/sci0/src/parsers/compression.ts
+++ libs/sci0/src/parsers/compression.ts
@@ -1,22 +1,25 @@
 import { decode as decodeHuffman } from '@4bitlabs/codecs/huffman';
 import { decode as decodeLzw } from '@4bitlabs/codecs/lzw';
 
-type SupportedAlgorithms = 0 | 1 | 2;
-const isSupported = (it: unknown): it is SupportedAlgorithms =>
+type Sci0Algorithms = 0 | 1 | 2;
+type Sci01Algorithms = 0 | 1;
+type DecompressFn = (bytes: Uint8Array) => Uint8Array;
+
+const sci0Supported = (it: unknown): it is Sci0Algorithms =>
   typeof it === 'number' && Number.isInteger(it) && [0, 1, 2].includes(it);
 
-const SCI0: Record<0 | 1 | 2, (bytes: Uint8Array) => Uint8Array> = {
+const sci01Supported = (it: unknown): it is Sci01Algorithms =>
+  typeof it === 'number' && Number.isInteger(it) && [0, 1].includes(it);
+
+const SCI0: Record<Sci0Algorithms, DecompressFn> = {
   0: (bytes) => bytes,
   1: (bytes) => decodeLzw(bytes, { literalWidth: 8, order: 'lsb' }),
   2: (bytes) => decodeHuffman(bytes),
 };
 
-const SCI01: Record<0 | 1 | 2, (bytes: Uint8Array) => Uint8Array> = {
+const SCI01: Record<Sci01Algorithms, DecompressFn> = {
   0: (bytes) => bytes,
   1: (bytes) => decodeHuffman(bytes),
-  2: () => {
-    throw new Error('unsupported');
-  },
 };
 
 export const decompress = (
@@ -26,12 +29,12 @@ export const decompress = (
 ): Uint8Array => {
   switch (mode) {
     case 'sci0': {
-      if (!isSupported(method))
+      if (!sci0Supported(method))
         throw new Error(`unsupported compression algorithm: ${method}`);
       return SCI0[method](bytes);
     }
     case 'sci01': {
-      if (!isSupported(method))
+      if (!sci01Supported(method))
         throw new Error(`unsupported compression algorithm: ${method}`);
       return SCI01[method](bytes);
     }
