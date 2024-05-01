24f2ab066c1f39dc5bbb7d730bc534850eaddaa7
J Holmes <32bitkid@gmail.com>
2024-05-01 16:53:38 -0600

    New: Added FontFace type export to sci0

--- libs/sci0/src/index.ts
+++ libs/sci0/src/index.ts
@@ -30,6 +30,7 @@ export { parseCursor, parseFont, parseView, parsePic };
 
 export { type Loop } from './models/view';
 export { type Cel } from './models/cel';
+export { type FontFace } from './models/font-face';
 
 /** @deprecated use parseCursor() instead */
 export const Cursor = {
