20114220677673b3a675555a0cb0b0b558623585
J Holmes <32bitkid@gmail.com>
2024-05-08 09:51:29 -0600

    New: adding support for sci01 compression-type "2"

--- libs/sci0/src/parsers/compression.ts
+++ libs/sci0/src/parsers/compression.ts
@@ -1,25 +1,27 @@
-import { decode as decodeHuffman } from '@4bitlabs/codecs/huffman';
-import { decode as decodeLzw } from '@4bitlabs/codecs/lzw';
+import { unpack as unpackHuffman } from '@4bitlabs/codecs/huffman';
+import { unpack as unpackLzw } from '@4bitlabs/codecs/lzw';
+import { unpack as unpackComp3 } from '@4bitlabs/codecs/comp3';
 
 type Sci0Algorithms = 0 | 1 | 2;
-type Sci01Algorithms = 0 | 1;
+type Sci01Algorithms = 0 | 1 | 2;
 type DecompressFn = (bytes: Uint8Array) => Uint8Array;
 
 const sci0Supported = (it: unknown): it is Sci0Algorithms =>
   typeof it === 'number' && Number.isInteger(it) && [0, 1, 2].includes(it);
 
 const sci01Supported = (it: unknown): it is Sci01Algorithms =>
-  typeof it === 'number' && Number.isInteger(it) && [0, 1].includes(it);
+  typeof it === 'number' && Number.isInteger(it) && [0, 1, 2].includes(it);
 
 const SCI0: Record<Sci0Algorithms, DecompressFn> = {
   0: (bytes) => bytes,
-  1: (bytes) => decodeLzw(bytes, { literalWidth: 8, order: 'lsb' }),
-  2: (bytes) => decodeHuffman(bytes),
+  1: (bytes) => unpackLzw(bytes, { literalWidth: 8, order: 'lsb' }),
+  2: (bytes) => unpackHuffman(bytes),
 };
 
 const SCI01: Record<Sci01Algorithms, DecompressFn> = {
   0: (bytes) => bytes,
-  1: (bytes) => decodeHuffman(bytes),
+  1: (bytes) => unpackHuffman(bytes),
+  2: (bytes) => unpackComp3(bytes),
 };
 
 export const decompress = (
