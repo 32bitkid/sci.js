b7a954ca3af516ba23e852552ababd3bcf81fb27
J Holmes <32bitkid@gmail.com>
2024-05-19 19:27:47 -0600

    initial commit of scishop

---
+++ apps/scishop/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Toolbar from './components/Toolbar.vue';
+import Sidebar from './components/Sidebar.vue';
+import stage from './components/Stage.vue';
+</script>
+
+<template>
+  <stage />
+  <toolbar />
+  <sidebar />
+  <Header />
+</template>
