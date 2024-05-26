a73bf268197fed635a1b6ae121312a876b1d1981
J Holmes <32bitkid@gmail.com>
2024-05-26 09:48:20 -0600

    WIP refactored low-level DrawCommand stacks into EditorCommand stacks

--- apps/scishop/src/data/picStore.ts
+++ apps/scishop/src/data/picStore.ts
@@ -1,17 +1,26 @@
-import { ref, shallowRef, unref } from 'vue';
+import { computed, ref, shallowRef, unref } from 'vue';
 
 import { DrawCommand } from '@4bitlabs/sci0';
 import { insert } from '../helpers/array-helpers.ts';
+import { EditorCommand } from '../models/EditorCommand.ts';
 
-const data: DrawCommand[] = [];
+const nextId = () => Math.random().toString(36).substring(2);
 
-const layersRef = shallowRef<DrawCommand[]>(data);
+const data: EditorCommand[] = [];
+
+const layersRef = shallowRef<EditorCommand[]>(data);
+const commandsRef = computed(() =>
+  unref(layersRef).flatMap((it) => it.commands),
+);
 const selectedCommandIdx = ref<number | null>(null);
 const topIdxRef = ref<number>(data.length - 1);
 
 export default {
   get layers() {
     return unref(layersRef);
+  },
+  get commands() {
+    return unref(commandsRef);
   },
   get cmdIdx() {
     return unref(selectedCommandIdx);
@@ -41,7 +50,11 @@ export const currentCommandStore = {
     const cmd = unref(currentCommandRef);
     if (cmd === null) return;
     currentCommandRef.value = null;
-    layersRef.value = insert(layersRef.value, unref(topIdxRef) + 1, cmd);
+    layersRef.value = insert(layersRef.value, unref(topIdxRef) + 1, {
+      id: nextId(),
+      type: cmd[0],
+      commands: [cmd],
+    });
     topIdxRef.value += 1;
   },
   abort() {
---
+++ apps/scishop/src/models/EditorCommand.ts
@@ -0,0 +1,34 @@
+import {
+  BrushCommand,
+  DrawCommand,
+  EmbeddedCelCommand,
+  FillCommand,
+  PolylineCommand,
+  SetPaletteCommand,
+  UpdatePaletteCommand,
+} from '@4bitlabs/sci0';
+
+export type BasicEditorCommand<T extends DrawCommand> = {
+  readonly id: symbol | number | string;
+  readonly type: T[0];
+  readonly name?: string;
+  readonly commands: readonly [T];
+};
+
+export type CompositeEditorCommand<T extends string> = {
+  readonly id: symbol | number | string;
+  readonly type: T;
+  readonly name?: string;
+  readonly commands: readonly (PolylineCommand | FillCommand | BrushCommand)[];
+};
+
+export type GroupEditorCommand = CompositeEditorCommand<'group'>;
+
+export type EditorCommand =
+  | GroupEditorCommand
+  | BasicEditorCommand<SetPaletteCommand>
+  | BasicEditorCommand<UpdatePaletteCommand>
+  | BasicEditorCommand<PolylineCommand>
+  | BasicEditorCommand<FillCommand>
+  | BasicEditorCommand<BrushCommand>
+  | BasicEditorCommand<EmbeddedCelCommand>;
