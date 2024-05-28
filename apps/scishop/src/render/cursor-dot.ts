0d88aecfb979a86b935e68e82b46473462743b74
J Holmes <32bitkid@gmail.com>
2024-05-28 10:29:28 -0600

    WIP: add path editing.

--- apps/scishop/src/render/cursor-dot.ts
+++ apps/scishop/src/render/cursor-dot.ts
@@ -6,9 +6,16 @@ export function cursorDot(
   size = 2,
 ) {
   ctx.save();
+
+  ctx.beginPath();
+  ctx.arc(x, y, size * 1.5, 0, Math.PI * 2);
+  ctx.fillStyle = 'black';
+  ctx.fill();
+
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fillStyle = 'white';
   ctx.fill();
+
   ctx.restore();
 }
