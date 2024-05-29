8c345e223f22eece64b6e97a3b0fc51612995cfb
J Holmes <32bitkid@gmail.com>
2024-05-29 13:34:58 -0600

    WIP: more core updates/features

--- apps/scishop/src/render/pline-skeleton.ts
+++ apps/scishop/src/render/pline-skeleton.ts
@@ -16,15 +16,18 @@ export function plineSkeleton(
     cPoints.map(([x, y]) => [x + 0.5, y + 0.5]),
   );
 
+  ctx.lineWidth = 1;
   ctx.beginPath();
   points.forEach(([x, y], idx) => ctx[idx ? 'lineTo' : 'moveTo'](x, y));
   ctx.stroke();
 
+  ctx.lineWidth = 1.5;
   points.forEach(([x, y], idx) => {
     ctx.save();
     ctx.translate(x, y);
     ctx.beginPath();
-    ctx.roundRect(-3, -3, 6, 6, 0.5);
+    ctx.clearRect(-2, -2, 4, 4);
+    ctx.roundRect(-2.5, -2.5, 5, 5, 0.5);
     ctx.stroke();
     if (highlights.includes(idx)) ctx.fill();
     ctx.restore();
