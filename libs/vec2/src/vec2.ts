87802da2355259564c1cc526eddbbaf1af338963
J Holmes <32bitkid@gmail.com>
2024-07-29 06:53:16 -0600

    Breaking: change Vec2 to be immutable, introduce MutableVec2

--- libs/vec2/src/vec2.ts
+++ libs/vec2/src/vec2.ts
@@ -1,25 +1,28 @@
 import { scalarLerp } from './maths';
 
-/** Two component vector */
-export type Vec2 = [number, number];
+/** An immutable two-component vector. */
+export type Vec2 = readonly [number, number];
 
-/** A readonly vector at (0 0) */
-export const ZERO: Readonly<Vec2> = [0, 0];
+/** A mutable two-component vector. */
+export type MutableVec2 = [number, number];
 
-/** Create a vector with component (X, Y), defaults to (0,0) */
-export const vec2 = (x: number = 0, y: number = 0): Vec2 => [x, y];
+/** A readonly vector at (0,0). */
+export const ZERO: Vec2 = [0, 0];
 
-/** Clone the source vector into a new vector */
-export const clone = (source: Readonly<Vec2>): Vec2 => assign(source, [0, 0]);
+/** Create a vector with component (X, Y), defaults to (0,0). */
+export const vec2 = (x: number = 0, y: number = 0): MutableVec2 => [x, y];
 
-/** Assign dest vector to the components of source vector */
-export const assign = (source: Readonly<Vec2>, dest: Vec2) => {
+/** Clone the source vector into a new vector. */
+export const clone = (source: Vec2): MutableVec2 => assign(source, [0, 0]);
+
+/** Assign dest vector to the components of source vector. */
+export const assign = (source: Vec2, dest: MutableVec2): MutableVec2 => {
   [dest[0], dest[1]] = source;
   return dest;
 };
 
-/** Set the components of out to X, Y */
-export const set = (x: number, y: number, out: Vec2) => {
+/** Set the components of out to X, Y. */
+export const set = (x: number, y: number, out: MutableVec2): MutableVec2 => {
   out[0] = x;
   out[1] = y;
   return out;
@@ -39,32 +42,61 @@ export const round = (
   method: RoundingMethodFn = Math.round,
   out: MutableVec2 = [0, 0],
 ): MutableVec2 => set(method(x), method(y), out);
+
+/**
+ * Adds vector A to vector B `(a + b)`, and puts the result in out.
+ *
+ * @param a
+ * @param b
+ * @param out
+ */
 export const add = (
-  [x0, y0]: Readonly<Vec2>,
-  [x1, y1]: Readonly<Vec2>,
-  out: Vec2 = [0, 0],
-) => set(x0 + x1, y0 + y1, out);
+  [x0, y0]: Vec2,
+  [x1, y1]: Vec2,
+  out: MutableVec2 = [0, 0],
+): MutableVec2 => set(x0 + x1, y0 + y1, out);
 
-/** Subtract vector second from the first vector, and put the result in out  */
+/**
+ * Subtract vector second from the first vector `(a - b)`, and put the result in out.
+ *
+ * @param a
+ * @param b
+ * @param out
+ */
 export const sub = (
-  [x0, y0]: Readonly<Vec2>,
-  [x1, y1]: Readonly<Vec2>,
-  out: Vec2 = [0, 0],
-): Vec2 => set(x0 - x1, y0 - y1, out);
+  [x0, y0]: Vec2,
+  [x1, y1]: Vec2,
+  out: MutableVec2 = [0, 0],
+): MutableVec2 => set(x0 - x1, y0 - y1, out);
 
-/** Scale a vector by a scalar, and put the result in out */
+/**
+ * Scale a vector by a scalar, and put the result in out.
+ *
+ * @param vec
+ * @param scalar
+ * @param out
+ */
 export const scale = (
-  [x, y]: Readonly<Vec2>,
+  [x, y]: Vec2,
   scalar: number,
-  out: Vec2 = [0, 0],
-): Vec2 => set(x * scalar, y * scalar, out);
+  out: MutableVec2 = [0, 0],
+): MutableVec2 => set(x * scalar, y * scalar, out);
 
-/** Get the dot-product of a vector */
-export const dot = (a: Readonly<Vec2>, b: Readonly<Vec2>) =>
-  a[0] * b[0] + a[1] * b[1];
+/** Get the dot-product of vector A and vector B. */
+export const dot = (a: Vec2, b: Vec2): number => a[0] * b[0] + a[1] * b[1];
 
-/** rotate Vec2 around [0,0] by theta */
-export const rotate = (source: Vec2, theta: number, out: Vec2 = source) => {
+/**
+ * Rotate Vec2 around the origin by &theta;.
+ *
+ * @param source
+ * @param theta The angle of rotation, in radians.
+ * @param out
+ */
+export const rotate = (
+  source: MutableVec2,
+  theta: number,
+  out: MutableVec2 = source,
+): MutableVec2 => {
   const sin = Math.sin(theta);
   const cos = Math.cos(theta);
   return ([out[0], out[1]] = [
@@ -73,13 +105,13 @@ export const rotate = (source: Vec2, theta: number, out: Vec2 = source) => {
   ]);
 };
 
-/** Project point P onto the line-segment AB‾ */
+/** Project point P onto the line-segment AB‾. */
 export const project = (
-  a: Readonly<Vec2>,
-  b: Readonly<Vec2>,
-  p: Readonly<Vec2>,
-  out: Vec2 = [0, 0],
-): Vec2 => {
+  a: Vec2,
+  b: Vec2,
+  p: Vec2,
+  out: MutableVec2 = [0, 0],
+): MutableVec2 => {
   const vecAB = sub(b, a);
   const vecAP = sub(p, a);
 
@@ -96,47 +128,49 @@ export const project = (
   return add(a, out, out);
 };
 
-/** Returns true if the vectors have the same components */
-export const isEqual = (a: Readonly<Vec2>, b: Readonly<Vec2>): boolean =>
+/** Returns true if the vectors have the same components. */
+export const isEqual = (a: Vec2, b: Vec2): boolean =>
   a[0] === b[0] && a[1] === b[1];
 
-/** Returns the distance-squared between vectors A and B */
-export const squaredDistanceBetween = (
-  a: Readonly<Vec2>,
-  b: Readonly<Vec2>,
-) => {
+/** Returns the distance-squared between vectors A and B. */
+export const squaredDistanceBetween = (a: Vec2, b: Vec2): number => {
   const [dx, dy] = sub(b, a);
   return dx ** 2 + dy ** 2;
 };
 
-/** Normalize the source vector into dest */
-export const normalize = (source: Readonly<Vec2>, dest: Vec2) =>
-  scale(source, 1 / length(source), dest);
-
-/** Returns the Euclidean distance between vectors A and B */
-export const distanceBetween = (a: Readonly<Vec2>, b: Readonly<Vec2>) =>
+/** Returns the Euclidean distance between vectors A and B. */
+export const distanceBetween = (a: Vec2, b: Vec2): number =>
   Math.sqrt(squaredDistanceBetween(a, b));
 
-/** Returns the squared-length of vector A from (0,0) */
-export const squaredLength = (a: Readonly<Vec2>) =>
+/** Returns the squared-length of vector A from (0,0). */
+export const squaredLength = (a: Vec2): number =>
   squaredDistanceBetween(ZERO, a);
 
-/** Returns the Euclidean length of vector A from (0,0) */
-export const length = (a: Readonly<Vec2>) => distanceBetween(ZERO, a);
+/** Returns the Euclidean length of vector A from (0,0). */
+export const length = (a: Vec2): number => distanceBetween(ZERO, a);
 
+/** Normalize the source vector into dest. */
+export const normalize = (source: Vec2, dest: MutableVec2): MutableVec2 =>
+  scale(source, 1 / length(source), dest);
+
+/**
+ *
+ * @param vec
+ * @param angleBrackets
+ */
 export const toString = (
-  [x, y]: Vec2,
+  [x, y]: MutableVec2,
   { angleBrackets = false }: { angleBrackets?: boolean } = {},
 ) =>
   `${angleBrackets ? '\u27E8' : '('}${x}, ${y}${angleBrackets ? '\u27E9' : ')'}`;
 
-/** Perform a linear-interpolation from vector A to vector B by t. t can be either a scalar or a Vec2. */
+/** Perform a linear-interpolation from vector A to vector B by `t`. `t` can be either a scalar or a Vec2. */
 export const lerp = (
-  a: Readonly<Vec2>,
-  b: Readonly<Vec2>,
-  t: number | Readonly<Vec2>,
-  out: Vec2 = [0, 0],
-): Vec2 => {
+  a: Vec2,
+  b: Vec2,
+  t: number | Vec2,
+  out: MutableVec2 = [0, 0],
+): MutableVec2 => {
   const [tX, tY] = typeof t === 'number' ? [t, t] : t;
   return set(scalarLerp(a[0], b[0], tX), scalarLerp(a[1], b[1], tY), out);
 };
