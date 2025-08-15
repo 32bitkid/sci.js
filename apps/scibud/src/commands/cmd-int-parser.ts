68faf60a8ec76fa6c44da991285eff6a3aa5205e
J Holmes <32bitkid@gmail.com>
2025-08-15 11:59:51 -0600

    Fix: use Number.isNaN

--- apps/scibud/src/commands/cmd-int-parser.ts
+++ apps/scibud/src/commands/cmd-int-parser.ts
@@ -3,6 +3,7 @@ import { InvalidArgumentError } from 'commander';
 export function cmdIntParser(value: string): number | false {
   if (/^\s*f(alse)?\s*$/i.test(value)) return false;
   const parsedValue = parseInt(value, 10);
-  if (isNaN(parsedValue)) throw new InvalidArgumentError('Not a number.');
+  if (Number.isNaN(parsedValue))
+    throw new InvalidArgumentError('Not a number.');
   return parsedValue;
 }
